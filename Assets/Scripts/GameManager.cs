3fbf4dd5a6d13ea704fa76e9654b17b22cedb6c7
José Enrique <5514134@alu.murciaeduca.es>
2024-01-17 13:56:27 +0100

    Día 17/01/2024

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,61 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public SceneManager[] escenariosCircuito;
-    // Start is called before the first frame update
-    void Start()
+    // Lista de escenarios que serán seleccionados
+    private List<string> escenariosCargados;
+    
+    // Lista de escenarios que serán los circuitos
+    [Header("ESCENARIOS CIRCUITOS")]
+    public List<string> escenariosCircuitos = new List<string>();
+    
+    //Lista de escenarios que serán los minijuegos
+    [Header("ESCENARIOS MINIJUEGOS")]
+    public List<string> escenariosMinijuegos = new List<string>();
+
+    // Método para guardar la lista de nombres de escenarios en PlayerPrefs
+    public void GuardarListaEscenarios()
     {
+        escenariosCargados = [esce]
         
+        
+        string listaCircuitos = JsonUtility.ToJson(escenariosCircuitos);
+        string listaMinijuegos = JsonUtility.ToJson(escenariosMinijuegos);
+
+        // Guardar la cadena JSON en PlayerPrefs
+        PlayerPrefs.SetString("ListaEscenarios", );
+        PlayerPrefs.Save();
+
+        Debug.Log("Lista de escenarios guardada");
     }
 
-    // Update is called once per frame
-    void Update()
+    // Método para cargar la lista de nombres de escenarios desde PlayerPrefs
+    public void CargarListaEscenarios()
     {
-        
+        // Obtener la cadena JSON de PlayerPrefs
+        string listaJson = PlayerPrefs.GetString("ListaEscenarios", "");
+
+        // Si hay una cadena JSON válida, convertirla a la lista
+        if (!string.IsNullOrEmpty(listaJson))
+        {
+            Debug.Log("Lista de escenarios cargada");
+        }
+        else
+        {
+            Debug.Log("No se encontró una lista de escenarios guardada");
+        }
+    }
+
+    // Método para cargar un escenario por su nombre
+    public void CargarEscenarioPorNombre(string nombreEscenario)
+    {
+        if (SceneManager.GetSceneByName(nombreEscenario) != null)
+        {
+            SceneManager.LoadScene(nombreEscenario);
+            Debug.Log("Escenario cargado: " + nombreEscenario);
+        }
+        else
+        {
+            Debug.LogError("El escenario '" + nombreEscenario + "' no existe");
+        }
     }
 }
