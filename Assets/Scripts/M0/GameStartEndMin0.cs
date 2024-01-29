685b40c9a6efa1973096d05f2d5b0a1529762a02
José Enrique <5514134@alu.murciaeduca.es>
2024-01-29 13:27:27 +0100

    Transición del minijuego 0 hecho
    Realice las transiciones del minijuego 0, aunque sea sencillo como esta ahora, pero esta pensado para modificarlo en un futuro. Iré subiendo de uno en uno para no ir perdiendolo por si las moscas

--- Assets/Scripts/M0/GameStartEndMin0.cs
+++ Assets/Scripts/M0/GameStartEndMin0.cs
@@ -8,53 +8,23 @@ public class GameStartEndMin0 : MonoBehaviour
 {
     public GameObject personaje;
 
-    private GameManager _gameManager;
+    private TransicionManagerMin0 _transicionManagerMin0;
 
-    public CanvasGroup canvas;
-    public TextMeshProUGUI textoPunto;
-
-    public float fadeDuration = 1f;
-    private float timer;
-    
     private bool acabado = false;
     
     // Start is called before the first frame update
     void Start()
     {
-        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        primeraTransicion();
+        _transicionManagerMin0 = GetComponent<TransicionManagerMin0>();
+        _transicionManagerMin0.transicionInicio();
     }
-
-    void primeraTransicion()
-    {
-        canvas.alpha = 1;
-        while (timer <= 5)
-        {
-            timer = timer + Time.deltaTime;
-        }
-        canvas.gameObject.SetActive(false);
-        timer = 0;
-        canvas.alpha = 0;
-
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
         if (acabado)
         {
-            if (timer == 0)
-            {
-                canvas.gameObject.SetActive(true);
-                StartCoroutine(PuntosTexto());
-            }
-            timer = timer + Time.deltaTime;
-            canvas.alpha = timer / fadeDuration;
-
-            if (timer > fadeDuration + 1)
-            {
-                InvokeRepeating("cambioEscenario", 5, 0);
-            }
+            _transicionManagerMin0.transicionFinal();
         }
     }
 
@@ -63,25 +33,6 @@ public class GameStartEndMin0 : MonoBehaviour
         if (other.gameObject == personaje)
         {
             acabado = true;
-            //_gameManager.siguienteEscenario();
         }
-    }
-
-    IEnumerator PuntosTexto()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(1f);
-            textoPunto.text = ".";
-            yield return new WaitForSeconds(1f);
-            textoPunto.text = "..";
-            yield return new WaitForSeconds(1f);
-            textoPunto.text = "...";
-        }
-    }
-
-    private void cambioEscenario()
-    {
-        _gameManager.siguienteEscenario();
     }
 }
