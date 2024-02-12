eb6272ed014296ae1899267a8ce9f2987680bc48
Pepeluisasecas <joseluis.jlca58@gmail.com>
2024-02-12 16:47:21 +0100

    añadido PlayerPrefab y NetworkRoomManager con las escenas para probar

--- Assets/Scripts/C1/GameStartEndCir1.cs
+++ Assets/Scripts/C1/GameStartEndCir1.cs
@@ -23,12 +23,6 @@ public class GameStartEndCir1 : MonoBehaviour
 
         textoPuntuacion = GameObject.Find("TextoPuntuacion").GetComponent<TextMeshProUGUI>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-
-        foreach (var puntuacionJugador in gameManager.puntuacionJugadores)
-        {
-            textoPuntuacion.text += "Jugador "+puntuacionJugador.Key+": "+puntuacionJugador.Value+"s";
-        }
-
     }
     
     // Update is called once per frame
